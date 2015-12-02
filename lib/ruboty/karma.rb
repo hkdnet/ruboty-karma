f6425b18d3993bc3ba2619ee20a1f5901b74c77c
hkdnet <hkdnet@users.noreply.github.com>
2015-12-03 00:54:08 +0900

    add list

--- lib/ruboty/karma.rb
+++ lib/ruboty/karma.rb
@@ -1,17 +1,28 @@
 require 'ruboty'
 require 'ruboty/karma/version'
+require 'ruboty/karma/actions/increase'
+require 'ruboty/karma/actions/list'
 
 module Ruboty
   module Handlers
     class Karma < Base
       on(
-        /@?(?<username>)\s*\+\+/m,
+        /@?(?<username>\S+)\s*\+\+/m,
         name: 'increment',
         description: "increment a user's karma"
+      )
+      on(
+        /list karma/m,
+        name: 'list',
+        description: "list all menber's karma"
       )
 
       def increment(message)
         Ruboty::Karma::Actions::Increase.new(message).call
+      end
+
+      def list(message)
+        Ruboty::Karma::Actions::List.new(message).call
       end
     end
   end
--- lib/ruboty/karma/actions/increase.rb
+++ lib/ruboty/karma/actions/increase.rb
@@ -3,12 +3,25 @@ module Ruboty
     module Actions
       class Increase < Ruboty::Actions::Base
         def call
-          username = message[:username]
+          name = message[:username]
+          add_user(name)
+          updated_karma = increment(name)
+          message.reply("#{name}: #{updated_karma}")
+        end
+
+        def add_user(name)
           brain = message.robot.brain
-          key = "karma-#{username}"
+          key = 'karma-list'
+          brain.data[key] ||= []
+          users = brain.data[key]
+          brain.data[key] = users.push(name).uniq
+        end
+
+        def increment(name)
+          brain = message.robot.brain
+          key = "karma-@#{name}"
           brain.data[key] ||= 0
           brain.data[key] += 1
-          message.reply("#{username}: #{brain.data[key]}")
         end
       end
     end
